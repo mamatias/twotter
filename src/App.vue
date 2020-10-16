3f318a6b579db23b59bdebf305af0068aa6a656e
matias <matiastorresrisso@gmail.com>
2020-10-15 23:24:32 -0300

    2nd commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    @{{user.username}}
+    <strong>
+      {{fullName}}
+    </strong>
+    <small>
+      {{user.email}} - Follower: {{followers}}
+    </small>
+    <button @click="followUser()">
+      Follow
+    </button>
+    <div id="banner">
+      <small><small><i>Msg: {{msg}}</i></small></small>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      followers:0,
+      user: {
+        username: 'mamatias',
+        name: 'Matías',
+        lastname: 'Torres',
+        email: 'matiastorresrisso@gmail.com',
+        isadmin: true
+      },
+      msg: ''
+    }
+  },
+  watch:{
+    followers(newVal, oldVal){
+      if (newVal > oldVal){
+        var d = new Date();
+        this.msg = 'Nuevo seguidor... '+
+        d.getFullYear()+'/'+
+        (d.getMonth()+1)+'/'+
+        d.getDate()+' '+
+        d.getHours()+':'+
+        d.getMinutes()+':'+
+        d.getSeconds();
+        console.log(d);
+      }
+    }
+  },
+  computed:{
+    fullName(){
+      return `${this.user.name} ${this.user.lastname}`;
+    }
+  },
+  methods:{
+    followUser(){
+      this.followers = this.followers + 1;
+    }
+  },
+  mounted(){
+    this.followUser();
   }
 }
 </script>
@@ -18,9 +67,14 @@ export default {
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /*-moz-osx-font-smoothing: grayscale;*/
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+#banner {
+  border: 2px;
 }
 </style>
